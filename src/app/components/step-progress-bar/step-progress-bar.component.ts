340f4245e9d590ec2cf1a1998cb1266253b2a51a
thiago <thiagosm.tech@gmail.com>
2023-09-03 12:05:43 -0300

    feat: update step progress bar

--- src/app/components/step-progress-bar/step-progress-bar.component.ts
+++ src/app/components/step-progress-bar/step-progress-bar.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'step-progress-bar',
   templateUrl: './step-progress-bar.component.html',
   styleUrls: ['./step-progress-bar.component.scss']
 })
-export class StepProgressBarComponent { }
+export class StepProgressBarComponent {
+
+  @Input() stepList!: string[];
+  @Input() currentStep: number = 0;
+
+}
